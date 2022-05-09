c87d08210388015ae7407caf0173bb2094683f16
Seung-wan <tmddhks0104@gmail.com>
2022-05-09 22:22:23 +0900

    Feat: Item component
    영화에 대한 정보를 담기 위해 컴포넌트를 작성하였습니다.

--- src/routes/Movie/Item.tsx
+++ src/routes/Movie/Item.tsx
@@ -1,22 +1,42 @@
+import { useState } from 'hooks'
+import { Search } from 'types/movie'
+
+// import { CheckIcon, StarIcon } from 'assets/svgs'
+
 import styles from './Item.module.scss'
+import SelectModal from './SelectModal'
 
 interface IProps {
+  ref?: any
+  item: Search
   img: string
   title: string
   year: string
   type: string
+  isFavorite: boolean
+  usedPage: string
 }
 
-const Item = ({ img, title, year, type }: IProps) => {
+const Item = ({ ref, item, img, title, year, type, isFavorite, usedPage }: IProps) => {
+  const [isClicked, setIsClicked] = useState(false)
+
+  const handleClickMovie = () => {
+    setIsClicked((prevState) => !prevState)
+  }
+
   return (
-    <article className={styles.movieCard}>
-      <img src={img} alt='Movie Poster' />
-      <div className={styles.movieDesc}>
-        <h3>{title}</h3>
-        <p className={styles.movieYear}>{year}</p>
-        <p className={styles.movieType}>{type}</p>
-      </div>
-    </article>
+    <li className={styles.itemContainer} ref={ref}>
+      <button className={styles.movieCard} type='button' onClick={handleClickMovie}>
+        <img src={img} alt='Movie Poster' />
+        <div className={styles.movieDesc}>
+          <h3>{title}</h3>
+          <p className={styles.movieYear}>{year}</p>
+          <p className={styles.movieType}>{type}</p>
+        </div>
+      </button>
+      {isFavorite && usedPage === '/' && <p className={styles.checkIcon}>즐겨찾기 됨</p>}
+      {isClicked && <SelectModal item={item} usedPage={usedPage} isFavorite={isFavorite} setIsClicked={setIsClicked} />}
+    </li>
   )
 }
 
