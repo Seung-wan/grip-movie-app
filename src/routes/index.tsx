40289bcae945f59c6671203f2471ea1839621dbc
Seung-wan <tmddhks0104@gmail.com>
2022-05-09 22:18:16 +0900

    Feat: react-router-dom v6
    라우트 기능을 적용하였습니다.

---
+++ src/routes/index.tsx
@@ -0,0 +1,24 @@
+import styles from './Routes.module.scss'
+// import TodoList from './TodoList'
+import Weather from './Weathers'
+import Movie from './Movie'
+import Favorites from './Movie/Favorites'
+import { RecoilRoot } from 'recoil'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+const App = () => {
+  return (
+    <div className={styles.app}>
+      <RecoilRoot>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Movie />} />
+            <Route path='/favorites' element={<Favorites />} />
+          </Routes>
+        </BrowserRouter>
+      </RecoilRoot>
+    </div>
+  )
+}
+
+export default App
