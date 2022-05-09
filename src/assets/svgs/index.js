d06ab75278f39f2494f70e2de4a2f95f31952c79
Seung-wan <tmddhks0104@gmail.com>
2022-05-09 22:22:40 +0900

    Add: StarIcon.svg

--- src/assets/svgs/index.js
+++ src/assets/svgs/index.js
@@ -3,3 +3,4 @@ export { ReactComponent as CheckIcon } from './check.svg'
 
 // moive
 export { ReactComponent as SearchIcon } from './movie/magnifying-glass-solid.svg'
+export { ReactComponent as StarIcon } from './movie/star-solid.svg'
