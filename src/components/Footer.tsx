57c1f92a027700f0b6ad3914d4d40e824fcfb926
Seung-wan <tmddhks0104@gmail.com>
2022-05-09 22:17:43 +0900

    Feat: Footer component
    공통으로 사용되는 Footer를 컴포넌트로 분리하였습니다.

---
+++ src/components/Footer.tsx
@@ -0,0 +1,28 @@
+import { Link } from 'react-router-dom'
+import styles from './Footer.module.scss'
+
+const Footer = () => {
+  return (
+    <footer>
+      <ul className={styles.linkContainer}>
+        <Link to='/'>
+          <li>
+            <button className={styles.linkButton} type='button'>
+              검색
+            </button>
+          </li>
+        </Link>
+
+        <Link to='/favorites'>
+          <li>
+            <button className={styles.linkButton} type='button'>
+              즐겨찾기
+            </button>
+          </li>
+        </Link>
+      </ul>
+    </footer>
+  )
+}
+
+export default Footer
