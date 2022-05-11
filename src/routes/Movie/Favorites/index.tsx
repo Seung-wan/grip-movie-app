6bb33ee6a5c8de48572fb6ff8b2622975b52c69c
Seung-wan <tmddhks0104@gmail.com>
2022-05-11 14:41:03 +0900

    add Header component

--- src/routes/Movie/Favorites/index.tsx
+++ src/routes/Movie/Favorites/index.tsx
@@ -1,16 +1,14 @@
 import { useEffect } from 'hooks'
 import store from 'store'
-import Item from '../Item'
-import { useRecoilState } from 'recoil'
-import { useLocation } from 'react-router-dom'
 import styles from './Favorites.module.scss'
+import { useRecoilState } from 'recoil'
 import { favoritesState } from '../recoil/movie'
-import { Search } from 'types/movie'
-import Footer from 'components/Footer'
+import MovieList from '../MovieList'
+import Footer from 'components/common/Footer'
+import Header from 'components/common/Header'
 
 const Favorites = () => {
   const [favoritesList, setFavoritesList] = useRecoilState(favoritesState)
-  const location = useLocation()
 
   useEffect(() => {
     const data = store.get('favorites')
@@ -22,33 +20,9 @@ const Favorites = () => {
 
   return (
     <div className={styles.container}>
-      <h1>Movie Searching App</h1>
+      <Header />
 
-      {/* 컴포넌트 분리 예정 */}
-      <h2>내 즐겨찾기</h2>
-      {!favoritesList?.length && <div className={styles.emptyMessage}>검색 결과가 없습니다.</div>}
-      <main className={styles.favoritesList}>
-        <ul>
-          {favoritesList &&
-            favoritesList.map((item: Search, idx: number) => {
-              const key = `${item.imdbID}-${idx}`
-              let isFavorite = false
-              if (favoritesList.find((favorite) => favorite.imdbID === item.imdbID)) isFavorite = true
-              return (
-                <Item
-                  item={item}
-                  key={key}
-                  img={item.Poster}
-                  title={item.Title}
-                  year={item.Year}
-                  type={item.Type}
-                  isFavorite={isFavorite}
-                  usedPage={location.pathname}
-                />
-              )
-            })}
-        </ul>
-      </main>
+      <MovieList movieList={favoritesList} />
 
       <Footer />
     </div>
