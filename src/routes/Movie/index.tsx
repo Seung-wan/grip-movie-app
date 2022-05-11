cca5ae390c95a1a1e088d51a1a49c978e486878c
Seung-wan <tmddhks0104@gmail.com>
2022-05-11 14:39:06 +0900

    change movieList to recoil atom

--- src/routes/Movie/index.tsx
+++ src/routes/Movie/index.tsx
@@ -1,42 +1,36 @@
-import { useLocation } from 'react-router-dom'
-import { useRecoilState } from 'recoil'
+import { ChangeEvent, FormEvent, Suspense } from 'react'
+import { useSetRecoilState, useRecoilState } from 'recoil'
 import store from 'store'
-import Item from './Item'
-import { useEffect, useState, useRef, useCallback } from 'hooks'
-import Footer from 'components/Footer'
-import { SearchIcon } from 'assets/svgs'
+import { useEffect, useState, useRef } from 'hooks'
+import Footer from 'components/common/Footer'
 import styles from './Movie.module.scss'
 import { Search } from 'types/movie'
 import { getMovieApi } from 'services/movie'
-import { favoritesState } from './recoil/movie'
+import { movieListState, favoritesState } from './recoil/movie'
+import MovieList from './MovieList'
+import Header from 'components/common/Header'
 
 const Movie = () => {
-  const [favoritesList, setFavoritesList] = useRecoilState(favoritesState)
-  const [movieList, setMovieList] = useState<Search[]>([])
+  const setFavoritesList = useSetRecoilState(favoritesState)
+  const [movieList, setMovieList] = useRecoilState<Search[]>(movieListState)
+  const [isLoading, setIsLoading] = useState(false)
   const [error, setError] = useState(false)
-  const [maxLength, setMaxLength] = useState<string>()
+  const [maxLength, setMaxLength] = useState('')
   const [page, setPage] = useState(0)
   const [searchText, setSearchText] = useState('')
   const [fixedText, setFixedText] = useState('')
+
   const loader = useRef(null)
-  const location = useLocation()
 
-  const handleObserver = useCallback((entries: IntersectionObserverEntry[]) => {
-    const target = entries[0]
-    if (target.isIntersecting) {
-      setPage((prev) => prev + 1)
-    }
-  }, [])
-
-  const handleChangeSearchText = (evt: React.ChangeEvent<HTMLInputElement>) => {
+  const handleChangeSearchText = (evt: ChangeEvent<HTMLInputElement>) => {
     setSearchText(evt.currentTarget.value)
   }
 
-  const handleSubmitForm = async (evt: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmitForm = async (evt: FormEvent<HTMLFormElement>) => {
     evt.preventDefault()
-    setPage(1)
 
     try {
+      setIsLoading(true)
       const data = await getMovieApi({
         apikey: String(process.env.REACT_APP_MOVIE_API_KEY),
         s: searchText,
@@ -45,6 +39,7 @@ const Movie = () => {
 
       setMaxLength(data.data.totalResults)
       setMovieList(data.data.Search)
+      setIsLoading(false)
     } catch (e) {
       setError(true)
     }
@@ -60,20 +55,32 @@ const Movie = () => {
     }
   }, [setFavoritesList])
 
+  const handleObserver = (entries: IntersectionObserverEntry[]) => {
+    const target = entries[0]
+    if (target.isIntersecting) {
+      setPage((prev) => prev + 1)
+    }
+  }
+
   useEffect(() => {
     const option = {
       root: null,
-      rootMargin: '20px',
+      rootMargin: '0px',
       threshold: 0,
     }
 
     const observer = new IntersectionObserver(handleObserver, option)
     if (loader.current) observer.observe(loader.current)
-  }, [handleObserver])
+  }, [])
+
+  useEffect(() => {
+    setPage(0)
+  }, [fixedText])
 
   useEffect(() => {
     const fetchData = async () => {
       try {
+        setIsLoading(true)
         const data = await getMovieApi({
           apikey: String(process.env.REACT_APP_MOVIE_API_KEY),
           s: fixedText,
@@ -81,6 +88,7 @@ const Movie = () => {
         })
 
         setMovieList((prevState) => [...prevState, ...data.data.Search])
+        setIsLoading(false)
       } catch (e) {
         setError(true)
       }
@@ -90,47 +98,18 @@ const Movie = () => {
     if (page > 1 && page <= Number(maxLength) / 10) {
       fetchData()
     }
-  }, [page, maxLength, fixedText])
+  }, [page])
 
   if (error) return <div>Error</div>
 
   return (
     <div className={styles.container}>
-      <h1>Movie Searching App</h1>
-
-      <form className={styles.searchForm} onSubmit={handleSubmitForm}>
-        <input type='text' placeholder='Search' value={searchText} onChange={handleChangeSearchText} />
-        <button type='submit'>
-          <SearchIcon className={styles.icon} />
-        </button>
-      </form>
-
-      {/* 컴포넌트 분리 예정 */}
-      <h2>Movie List</h2>
-      {!movieList && <div className={styles.emptyMessage}>검색 결과가 없습니다.</div>}
-      <main className={styles.movieList}>
-        <ul>
-          {movieList?.map((item, idx) => {
-            const key = `${item.imdbID}-${idx}`
-            let isFavorite = false
-            if (favoritesList.find((favorite) => favorite.imdbID === item.imdbID)) isFavorite = true
-            return (
-              <Item
-                key={key}
-                item={item}
-                img={item.Poster}
-                title={item.Title}
-                year={item.Year}
-                type={item.Type}
-                isFavorite={isFavorite}
-                usedPage={location.pathname}
-              />
-            )
-          })}
-        </ul>
-        {movieList && <div ref={loader} />}
-      </main>
-
+      <Header
+        searchText={searchText}
+        handleChangeSearchText={handleChangeSearchText}
+        handleSubmitForm={handleSubmitForm}
+      />
+      <MovieList movieList={movieList} loader={loader} fixedText={fixedText} isLoading={isLoading} />
       <Footer />
     </div>
   )
