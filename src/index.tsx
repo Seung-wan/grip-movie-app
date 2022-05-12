ef2eadc1a80516deae3d098f3ce838926b8cefe7
Seung-wan <tmddhks0104@gmail.com>
2022-05-12 23:26:00 +0900

    Style: Change import order

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './styles/index.scss'
+
 import Routes from './routes'
+import './styles/index.scss'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+
 root.render(
   <React.StrictMode>
     <Routes />
