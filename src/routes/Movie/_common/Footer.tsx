6635325dadc3e2c48b048541a2c9bde23dfa13ef
Seung-wan <tmddhks0104@gmail.com>
2022-05-15 01:24:46 +0900

    Add Nav tag

--- src/routes/Movie/_common/Footer.tsx
+++ src/routes/Movie/_common/Footer.tsx
@@ -5,23 +5,25 @@ import styles from './footer.module.scss'
 const Footer = () => {
   return (
     <footer>
-      <ul className={styles.linkContainer}>
-        <Link to='/'>
-          <li>
-            <button className={styles.linkButton} type='button'>
-              검색
-            </button>
-          </li>
-        </Link>
+      <nav>
+        <ul className={styles.linkContainer}>
+          <Link to='/'>
+            <li>
+              <button className={styles.linkButton} type='button'>
+                검색
+              </button>
+            </li>
+          </Link>
 
-        <Link to='/favorites'>
-          <li>
-            <button className={styles.linkButton} type='button'>
-              즐겨찾기
-            </button>
-          </li>
-        </Link>
-      </ul>
+          <Link to='/favorites'>
+            <li>
+              <button className={styles.linkButton} type='button'>
+                즐겨찾기
+              </button>
+            </li>
+          </Link>
+        </ul>
+      </nav>
     </footer>
   )
 }
