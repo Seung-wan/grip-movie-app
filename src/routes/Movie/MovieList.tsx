fdd8fcca07e922254543861e8406175fcc4f8b0e
Seung-wan <tmddhks0104@gmail.com>
2022-05-14 22:46:13 +0900

    Feat: Error message

--- src/routes/Movie/MovieList.tsx
+++ src/routes/Movie/MovieList.tsx
@@ -2,36 +2,41 @@ import { useRef, useEffect } from 'hooks'
 import { useLocation } from 'react-router-dom'
 import Item from './Item'
 import styles from './movieList.module.scss'
-import { useRecoilValue } from 'recoil'
-import { favoritesState } from './recoil/movie'
-import { Search } from 'types/movie'
+import { useRecoilState, useRecoilValue } from 'recoil'
+import { favoritesState, movieListState } from './recoil/movie'
+import { ISearch } from 'types/movie'
 import { TailSpin } from 'react-loader-spinner'
 
 interface Props {
-  movieList: Search[]
-  loader?: React.MutableRefObject<null>
+  movieList: ISearch[]
+  scrollRef?: React.MutableRefObject<HTMLLIElement | null>
   fixedText?: string
-  isLoading?: boolean
+  loading?: boolean
+  errorMessage?: string | undefined
 }
 
-const MovieList = ({ movieList, loader, fixedText, isLoading }: Props) => {
+const MovieList = ({ movieList, fixedText, loading, scrollRef, errorMessage }: Props) => {
   const favoritesList = useRecoilValue(favoritesState)
+  // const [movieList, setMovieList] = useRecoilState(movieListState)
   const location = useLocation()
-  const movieListTopRef = useRef<null | HTMLDivElement>(null)
+  const movieListTopRef = useRef<HTMLDivElement>(null)
+
   const scrollToBottom = () => {
     if (movieListTopRef.current !== null) {
       movieListTopRef.current.scrollIntoView({ behavior: 'smooth' })
     }
   }
-
   useEffect(() => {
     scrollToBottom()
   }, [fixedText])
+
+  if (errorMessage) return <div className={styles.errorMessage}>검색 결과가 없습니다.</div>
+
   return (
     <main>
       {location.pathname === '/favorites' ? <h2>내 즐겨찾기</h2> : <h2>Movie List</h2>}
 
-      {!movieList && <div className={styles.emptyMessage}>검색 결과가 없습니다.</div>}
+      {!loading && movieList.length === 0 && <div className={styles.emptyMessage}>검색 결과가 없습니다.</div>}
 
       <ul className={styles.movieList}>
         <div ref={movieListTopRef} />
@@ -46,13 +51,10 @@ const MovieList = ({ movieList, loader, fixedText, isLoading }: Props) => {
               year={item.Year}
               type={item.Type}
               isFavorite={!!favoritesList.find((favorite) => favorite.imdbID === item.imdbID)}
-              usedPage={location.pathname}
             />
           )
         })}
-        {isLoading && <TailSpin />}
-
-        {movieList && <div ref={loader} />}
+        <li ref={scrollRef}>{loading && <TailSpin />}</li>
       </ul>
     </main>
   )
